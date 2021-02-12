65f72cd099c7c6291d83c9acdc7a479a0201be76
byma <git@by-ma.art>
2021-02-12 18:00:54 +0000

    Initial commit.

---
+++ contracts/Curve.sol
@@ -0,0 +1,111 @@
+// SPDX-License-Identifier: MIT
+// Based on https://github.com/simondlr/neolastics/blob/master/packages/hardhat/contracts/Curve.sol
+
+pragma solidity ^0.7.0;
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/IERC721.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/IERC721Enumerable.sol";
+
+
+abstract contract Curve is IERC721, IERC721Enumerable {
+    using SafeMath for uint256;
+    // linear bonding curve
+    // 99.5% going into reserve.
+    // 0.5% going to creator.
+
+    // this is currently 0.5%
+    uint256 public initMintPrice = 0.02 ether; // at 0
+    uint256 public initBurnPrice = 0.000995 ether; // at 1
+
+    // You technically do not need to keep tabs on the reserve
+    // because it uses linear pricing
+    // but useful to know off-hand. Especially because this.balance might not be the same as the actual reserve
+    uint256 public reserve;
+
+    address payable public creator;
+
+    event Minted(uint256 indexed tokenId, uint256 indexed pricePaid, uint256 indexed reserveAfterMint);
+    event Burned(uint256 indexed tokenId, uint256 indexed priceReceived, uint256 indexed reserveAfterBurn);
+
+    /*
+    todo: 
+    flash minting protection
+    front-running exploits
+    */
+    constructor (address payable _creator) {
+        creator = _creator;
+        reserve = 0;
+    }
+
+    /*
+    With one mint front-runned, a front-runner will make a loss.
+    With linear price increases of 0.001, it's not profitable.
+    BECAUSE it costs 0.012 ETH at 50 gwei to mint (storage/smart contract costs) + 0.5% loss from creator fee.
+    It becomes more profitable to front-run if there are multiple buys that can be spotted
+    from multiple buyers in one block. However, depending on gas price, it depends how profitable it is.
+    Because the planned buffer on the front-end is 0.01 ETH, it's not profitable to front-run any normal amounts.
+    Unless, someone creates a specific contract to start bulk minting.
+    To curb speculation, users can only mint one per transaction (unless you create a separate contract to do this).
+    Thus, ultimately, at this stage, while front-running can be profitable,
+    it is not generally feasible at this small scale.
+    Thus, for the sake of usability, there's no additional locks here for front-running protection.
+    A lock would be to have a transaction include the current price:
+    But that means, that only one neolastic per block would be minted (unless you can guess price rises).
+    */
+    function mint() public virtual payable returns (uint256 _tokenId) {
+        // you can only mint one at a time.
+        require(msg.value > 0, "C: No ETH sent");
+
+        uint256 mintPrice = getCurrentPriceToMint();
+        require(msg.value >= mintPrice, "C: Not enough ETH sent");
+
+        // mint first to increase supply.
+        uint256 tokenId = onMint();
+
+        // disburse
+        uint256 reserveCut = getReserveCut();
+        reserve = reserve.add(reserveCut);
+        creator.transfer(mintPrice.sub(reserveCut)); // 0.5%
+
+        if(msg.value.sub(mintPrice) > 0) {
+            msg.sender.transfer(msg.value.sub(mintPrice)); // excess/padding/buffer
+        }
+
+        emit Minted(tokenId, mintPrice, reserve);
+
+        return tokenId; // returns tokenId in case its useful to check it
+    }
+
+    function burn(uint256 tokenId) public virtual {
+        require(msg.sender == this.ownerOf(tokenId), "not-owner");
+
+        uint256 burnPrice = getCurrentPriceToBurn();
+        onBurn(tokenId);
+
+        reserve = reserve.sub(burnPrice);
+        msg.sender.transfer(burnPrice);
+
+        emit Burned(tokenId, burnPrice, reserve);
+    }
+
+    // if supply 0, mint price = 0.001
+    function getCurrentPriceToMint() public virtual view returns (uint256) {
+        uint256 mintPrice = initMintPrice.add(this.totalSupply().mul(initMintPrice));
+        return mintPrice;
+    }
+
+    // helper function for legibility
+    function getReserveCut() public virtual view returns (uint256) {
+        return getCurrentPriceToBurn();
+    }
+
+    // if supply 1, then burn price = 0.000995
+    function getCurrentPriceToBurn() public virtual view returns (uint256) {
+        uint256 burnPrice = this.totalSupply().mul(initBurnPrice);
+        return burnPrice;
+    }
+
+    function onMint() internal virtual returns (uint256 tokenId);
+    function onBurn(uint256 tokenId) internal virtual;
+}
---
+++ contracts/NFT.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+import "openzeppelin-solidity/contracts/token/ERC721/IERC721.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/IERC721Metadata.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/IERC721Enumerable.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/IERC721Receiver.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-solidity/contracts/introspection/ERC165.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "./Curve.sol";
+
+
+contract NFT is ERC721("Go forth and bond", "BONDLOVE"), Curve {
+    using SafeMath for uint256;
+
+    uint256 MAX_TOKENS_PER_PIECE = 10;
+
+    // Each token owns one piece. Only a limited number of tokens can own a
+    // single piece.  A new piece is created once the previous one is full.
+    struct Piece {
+        uint256[] tokenIds;
+        uint256 startedAt;
+        uint256 endedAt;
+        uint256 scheduledEndAt;
+    }
+
+    mapping(uint256 => Piece) pieces;
+    uint256 public numPieces = 0;
+
+    constructor() Curve(msg.sender) {
+    }
+
+    // End the old piece, add a new one
+    function makeNewPiece() private returns (Piece storage) {
+        uint256 newSegmentLength;
+        if (numPieces > 0) {
+            Piece storage currentPiece = getCurrentPiece();
+            currentPiece.endedAt = block.timestamp;
+            uint256 lastSegmentLength = currentPiece.endedAt.sub(currentPiece.startedAt);
+
+            // If we filled up early, make the next segment longer. If we ended late, make the next segment longer.
+            if (currentPiece.endedAt < currentPiece.scheduledEndAt) {
+                newSegmentLength = lastSegmentLength.mul(75).div(100);
+            } else {
+                newSegmentLength = lastSegmentLength.mul(125).div(100);
+            }
+        }
+        else {
+            // Initial segment length
+            newSegmentLength = 5 days;
+        }
+
+        // Increase the piece count
+        numPieces = numPieces + 1;
+        uint256 pieceId = numPieces;
+
+        pieces[pieceId].startedAt = block.timestamp;
+        pieces[pieceId].scheduledEndAt = block.timestamp.add(newSegmentLength);
+
+        return pieces[pieceId];
+    }
+
+    function getCurrentPiece() private returns (Piece storage) {
+        return pieces[numPieces];
+    }
+
+    function onMint() internal override returns (uint256 tokenId) {
+        tokenId = this.totalSupply() + 1;
+        _mint(msg.sender, tokenId);
+
+        Piece storage currentPiece = getCurrentPiece();
+
+        // We generate a new piece if the old one is full, or if it's segment timed out.
+        bool isPieceFull = currentPiece.tokenIds.length >= MAX_TOKENS_PER_PIECE;
+        bool hasTimedOut = block.timestamp >= currentPiece.scheduledEndAt;
+
+        if (isPieceFull || hasTimedOut) {
+            currentPiece = makeNewPiece();
+        }
+
+        currentPiece.tokenIds.push(tokenId);
+    }
+
+    function onBurn(uint256 tokenId) internal override {
+        _burn(tokenId);
+    }
+}
